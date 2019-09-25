741c5afdf398ce8c21f2a60be5d5f4a58de79196
nor-ko-hi-jp <saandna111@gmail.com>
2019-09-25 23:58:08 +0900

    add ignorePattern

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(test).ts?(x)']
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(test).ts?(x)'],
+  transformIgnorePatterns: ['node_modules']
 }
