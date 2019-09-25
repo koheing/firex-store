5521cb40088ad9ba2ed7f7ac15fcadca8c7750c0
nor-ko-hi-jp <saandna111@gmail.com>
2019-09-26 00:09:08 +0900

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(test).ts?(x)'],
-  transformIgnorePatterns: ['node_modules']
+  transformIgnorePatterns: ['node_modules/(?!(jest-test))'],
+  globals: {
+    'ts-jest': {
+      babelConfig: true
+    }
+  }
 }
