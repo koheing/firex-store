c478204050741b0f31b662fac8c503f67d51a64b
nor-ko-hi-jp <saandna111@gmail.com>
2019-09-29 21:37:35 +0900

    add callCollectionMutation

--- tests/__tests__/service/helpers/mutation-handlers.test.ts
+++ tests/__tests__/service/helpers/mutation-handlers.test.ts
@@ -2,6 +2,7 @@ import {
   callDocumentMutation, callCollectionMutation
 } from '../../../../src/service/helpers/mutation-handlers'
 import{ MockDocumentSnapshot } from '../../../mocks/mock-document-snapshot'
+import{ MockQuerySnapshot } from '../../../mocks/mock-query-snapshot'
 import { Payload } from '../../../../src/models'
 
 describe('callDocumentMutation', () => {
@@ -38,6 +39,27 @@ describe('callDocumentMutation', () => {
     expect(callMutation.mock.calls[0][1].data.docId).toEqual('test0001')
     expect(callMutation.mock.calls[0][1].data.count).toEqual(0)
     expect(callMutation.mock.calls[0][1].isLast).toBeFalsy()
+    expect(afterMutationCalled).toHaveBeenCalled()
+  })
+})
+
+describe('callCollectionMutation', () => {
+  it('documentMutation called, type and isLast are default value', () => {
+    const callMutation = jest.fn((type: string, payload: Payload) => payload)
+    const afterMutationCalled = jest.fn()
+    const notifyNotFound = jest.fn()
+    callCollectionMutation({
+      snapshot: new MockQuerySnapshot(true,
+        [
+          new MockDocumentSnapshot(),
+          new MockDocumentSnapshot(true, { name: 'test0002', count: 1 })
+        ]),
+      callMutation,
+      afterMutationCalled,
+      notifyNotFound
+    })
+
+    expect(callMutation).toHaveBeenCalled()
     expect(afterMutationCalled).toHaveBeenCalled()
   })
 })
