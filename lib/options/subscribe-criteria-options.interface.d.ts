a46e343fe4e0c622e89398d9adfaadbb76d549b2
North Ko-hi <55611095+nor-ko-hi-jp@users.noreply.github.com>
2019-10-03 20:10:41 +0900

    Fix document (#24)
    * fix readme and interface doc
    
    * build

--- lib/options/subscribe-criteria-options.interface.d.ts
+++ lib/options/subscribe-criteria-options.interface.d.ts
@@ -2,7 +2,7 @@ import { CriteriaOptions } from './criteria-options.interface';
 import { AfterMutationCalled, NotFoundHandler } from '../types';
 export interface SubscribeCriteriaOptions<T> extends CriteriaOptions<T> {
     /**
-     * @param afterMutationCalled convert subscribed data to something, if defined
+     * @param afterMutationCalled This method called after mutation called
      *   - `subscribeFirestore` and `subscribeFirestoreActions` only.
      *   - type: (payload: Payload) => void
      *     - payload = { data: { docId: string, [key: string]: any }, isLast: boolean }
