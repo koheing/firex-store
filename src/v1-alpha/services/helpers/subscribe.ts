35ac533e03be090383915e48f90d73a942bdc128
nor-ko-hi-jp <saandna111@gmail.com>
2019-10-22 22:19:48 +0900

    fix: remove redundant doc

--- src/v1-alpha/services/helpers/subscribe.ts
+++ src/v1-alpha/services/helpers/subscribe.ts
@@ -69,17 +69,3 @@ export const subscribeFirestoreDocument = <T = any>({
 
   state[FIREX_DOCUMENT_UNSUBSCRIBER] = unsubscriber
 }
-
-/**
- * @description subscribe firestore data to state property
- * @param state vuex's state
- * @param commit vuex's commit
- * @param ref firebase.firestore.DocumentReference | firebase.firestore.CollectionReference | firebase.firestore.Query
- * @param options optional methods. can undefined
- *   - mapper
- *   - errorHandler
- *   - completionHandler
- *   - notFoundHandler
- *   - afterMutationCalled
- *   - onCompleted `deprecated`
- */
