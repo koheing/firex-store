7350133c20171738cdb449f5fb2be35063300ef4
nor-ko-hi-jp <saandna111@gmail.com>
2019-10-27 23:24:34 +0900

    docs: add return value description

--- src/creators/from.creator.ts
+++ src/creators/from.creator.ts
@@ -4,6 +4,7 @@ import { FirestoreReaderServiceFactory } from '../factories'
 /**
  * @description return factory of FirestoreSubscriber and FirestoreFinder
  * @param ref: firebase.firestore.DocumentReference | firebase.firestore.CollectionReference | firebase.firestore.Query
+ * @returns FirestoreReaderServiceFactory
  */
 export const from = (ref: FirestoreRef) =>
   new FirestoreReaderServiceFactory(ref)
