67a5832557a85d62619c72962237d7cd10e3e441
nor-ko-hi-jp <saandna111@gmail.com>
2019-11-10 11:51:29 +0900

    test: coverage 95.4%

---
+++ tests/__tests__/repositories/helpers/firestore-set-transaction.test.ts
@@ -0,0 +1,43 @@
+import { transactionOfSet } from '../../../../src/repositories/helpers'
+import { MockDocumentReference } from '../../../mocks/mock-document-reference'
+import { MockDocumentSnapshot } from '../../../mocks/mock-document-snapshot'
+import { MockTransaction } from '../../../mocks/mock-transaction'
+import { AppError } from '../../../../src/models'
+import * as flushPromises from 'flush-promises'
+
+
+describe('transactionOfSet', () => {
+  it('set succeeded', async (done) => {
+    const transaction = new MockTransaction()
+    const mockSet = jest.fn()
+    transaction.set = mockSet
+    const result = await transactionOfSet({
+      ref: new MockDocumentReference(Promise.resolve(new MockDocumentSnapshot(false, { name: 'test', count: 1 }))),
+      data: { name: 'test' },
+      merge: true,
+      transaction
+    })
+    await flushPromises()
+    expect(result).toBeUndefined()
+    expect(transaction.set).toHaveBeenCalled()
+    jest.clearAllMocks()
+    done()
+  })
+
+  it('error occured', async (done) => {
+    const transaction = new MockTransaction() as firebase.firestore.Transaction
+    const errorHandler = jest.fn(() => ({ message: 'error' }) as AppError)
+    const result = await transactionOfSet({
+      ref: new MockDocumentReference(Promise.resolve(new MockDocumentSnapshot(true, { name: 'test', count: 1 }))),
+      data: { name: 'test' },
+      merge: true,
+      transaction,
+      errorHandler
+    })
+    await flushPromises()
+    expect(result).toHaveProperty('message')
+    expect(errorHandler).toHaveBeenCalled()
+    jest.clearAllMocks()
+    done()
+  })
+})
