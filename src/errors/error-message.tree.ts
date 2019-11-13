1139553a2f1de2fd590bd84ee98f9ef0b2fde8cc
North Ko-hi <55611095+nor-ko-hi-jp@users.noreply.github.com>
2019-11-13 20:53:39 +0900

    Fix wrong error (#49)
    * fix: wrong error
    
    * fix: descriptions
    
    * fix: descriptions

---
+++ src/errors/error-message.tree.ts
@@ -0,0 +1,5 @@
+export const errorMessageTree = {
+  BIND_TO_METHOD_NOT_CALLED: 'You need to call bindTo method before call it.',
+  UNSUBSCRIBE_METHOD_NOT_CALLED:
+    'Unsubscribe method have been called, but not unsubscribed.'
+}
