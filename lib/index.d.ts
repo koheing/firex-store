6bdad8690a623d32f759b8a702e073b6f24164f1
nor-ko-hi-jp <saandna111@gmail.com>
2019-11-27 19:42:54 +0900

    feat: add toJson test

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,7 @@
 export * from './store';
 export { FirestoreFinder, FirestoreSubscriber, FirestoreUnsubscriber, FirestoreAdder, FirestoreMergeSetter, FirestoreSetter, FirestoreDeleter } from './services';
 export { Mapper, AfterMutationCalled, ErrorHandler, CompletionHandler, NotFoundHandler } from './types';
-export { Payload, DocumentResult, AppError } from './models';
+export { Payload, DocumentResult, AppError, FirestoreMapper } from './models';
 export { from, on, to } from './creators';
 import * as v0 from './v0';
 export { v0 };
