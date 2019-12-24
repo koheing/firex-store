d8011513f866f40b02826c10132ad674a948eb8b
nor-ko-hi-jp <saandna111@gmail.com>
2019-12-24 20:15:46 +0900

    test: fix test bug

---
+++ tests/__tests__/services/helpers/create-mutation.test.ts
@@ -0,0 +1,22 @@
+// import { createMutation } from '../../../../src/services/helpers'
+// import { Store } from 'vuex'
+// import { firestoreMutations } from '../../../../src'
+
+// describe('createMutation', () => {
+//   let store: Store<any>
+
+//   beforeEach(() => {
+//     store = new Store<any>({
+//       state: {
+//         count: 0,
+//         counts: []
+//       },
+//       mutations: {
+//         ...firestoreMutations('all')
+//       },
+//       actions: {
+        
+//       }
+//     })
+//   })
+// })
