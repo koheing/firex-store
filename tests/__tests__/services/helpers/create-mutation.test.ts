81b8147ea78d9904383a82c9c6ec2e16ad03499e
nor-ko-hi-jp <saandna111@gmail.com>
2019-12-24 20:35:38 +0900

    test: add createMutation test

--- tests/__tests__/services/helpers/create-mutation.test.ts
+++ tests/__tests__/services/helpers/create-mutation.test.ts
@@ -1,22 +1,41 @@
-// import { createMutation } from '../../../../src/services/helpers'
-// import { Store } from 'vuex'
-// import { firestoreMutations } from '../../../../src'
+import { createMutation } from '../../../../src/services/helpers'
+import { Store } from 'vuex'
+import * as Vuex from 'vuex'
+import { firestoreMutations } from '../../../../src'
+import { createLocalVue } from '@vue/test-utils'
 
-// describe('createMutation', () => {
-//   let store: Store<any>
+const Vue = createLocalVue()
 
-//   beforeEach(() => {
-//     store = new Store<any>({
-//       state: {
-//         count: 0,
-//         counts: []
-//       },
-//       mutations: {
-//         ...firestoreMutations('all')
-//       },
-//       actions: {
-        
-//       }
-//     })
-//   })
-// })
+Vue.use(Vuex)
+
+describe('createMutation', () => {
+  let store: Store<any>
+
+  beforeEach(() => {
+    store = new Store<any>({
+      state: {
+        document: { count: 0 },
+        array: []
+      },
+      mutations: {
+        ...firestoreMutations('all')
+      },
+      actions: {
+        documentAdd: ({ commit }) => {
+          const callMutation = createMutation({
+            mutationType: 'collection',
+            commit
+          })
+
+          callMutation('added', { data: { count: 1 }, statePropName: 'counts' })
+        }
+      }
+    })
+  })
+
+  it('document ADD', () => {
+    store.dispatch('documentAdd')
+
+    expect(store.state['counts'][0].count).toEqual(1)
+  })
+})
