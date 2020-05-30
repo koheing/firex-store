000fcb5b942110d20113fcc40616ab74344c8964
North Ko-hi <55611095+nor-ko-hi-jp@users.noreply.github.com>
2020-05-30 10:46:52 +0900

    fix:[package.json] bump up dev libraries version (#75)
    * fix:[package.json] bump up dev library version
    
    * 1.6.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/ban-ts-ignore': 'off'
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off'
   }
 }
